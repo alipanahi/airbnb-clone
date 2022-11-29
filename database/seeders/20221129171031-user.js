38bceae66a219b145ca0c8c58e5a0a45bfbf66fc
sajadoncode <sajadoncode@gmail.com>
2022-11-29 18:13:59 +0100

    add user seed

---
+++ database/seeders/20221129171031-user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+     await queryInterface.bulkInsert('Users', [{
+       name: 'Luca',
+       lastName: 'Lucai',
+       type: 'owner',
+       city_id: '1',
+       createdAt: new Date(),
+       updatedAt: new Date()
+     }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
