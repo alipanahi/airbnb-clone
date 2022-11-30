102d71ad7b0a13c8c14c498e692ef21eb9f37876
alipanahi <p.alihussain@gmail.com>
2022-11-30 17:43:35 +0100

    env variables

--- database/config/config.mjs
+++ database/config/config.mjs
@@ -6,7 +6,7 @@ const config = {
   "development": {
     "username": process.env.DB_USER,
     "password": process.env.DB_PASSWORD,
-    "database": "airbnb-clone",
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
