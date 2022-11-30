11ee53a88f7b1c0c6216c627343f3860eaa07138
sajadoncode <sajadoncode@gmail.com>
2022-11-30 17:29:41 +0100

    index page for sellers

---
+++ pages/sellers/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+const IndexPage = (props) => {
+  return (
+    <>
+      {/* here is first nav component */}
+      <div className="container">
+        <h1>This is seller index file</h1>
+        <div className="row"></div>
+      </div>
+    </>
+  );
+};
+
+export default IndexPage;
