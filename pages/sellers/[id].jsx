7dfb0a86bbc6a0bb9047154b040d5519db4688de
sajadoncode <sajadoncode@gmail.com>
2022-11-30 17:30:21 +0100

    id page for sellers

---
+++ pages/sellers/[id].jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+const ShowPage = (props) => {
+  return (
+    <>
+      {/* here is first nav component */}
+      <div className="container">
+        <h1>This is seller show file</h1>
+        <div className="row"></div>
+      </div>
+    </>
+  );
+};
+
+export default ShowPage;
