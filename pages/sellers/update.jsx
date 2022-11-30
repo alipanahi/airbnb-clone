e417d661769da478524b663251529950f35110fb
sajadoncode <sajadoncode@gmail.com>
2022-11-30 17:30:53 +0100

    update page for sellers

---
+++ pages/sellers/update.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+const UpdatePage = (props) => {
+  return (
+    <>
+      {/* here is first nav component */}
+      <div className="container">
+        <h1>This is seller update file</h1>
+        <div className="row"></div>
+      </div>
+    </>
+  );
+};
+
+export default UpdatePage;
