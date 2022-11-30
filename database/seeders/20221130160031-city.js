97ea0ecfb333519a8d60e828c1a6a21449fdce10
sajadoncode <sajadoncode@gmail.com>
2022-11-30 17:04:53 +0100

    seedrs for city

---
+++ database/seeders/20221130160031-city.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Cities', [{
+      name: 'rome',
+      country_name: 'Rome',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
