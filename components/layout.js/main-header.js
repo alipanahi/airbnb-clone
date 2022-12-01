ac633b6c64e19012f2e67b172626c61852419c31
Fraidon Farahmand <fraidon82@yahoo.com>
2022-12-01 12:42:39 +0100

    navbar new style applied

--- components/layout.js/main-header.js
+++ components/layout.js/main-header.js
@@ -4,27 +4,28 @@ import classes from './main-header.module.css';
 function MainHeader() {
     return (
         <header className={classes.navbar}>
-            <div className={classes.logo}>
-                <Link href="/">Airbnb  </Link>
+
+            <div className={classes.airbnbLogo}>
+                <Link href="/">Airbnb </Link>
             </div>
-            <div className={classes.logo}>
+            <div className={classes.navbtn}>
                 <Link href="/profile">Profile  </Link>
             </div>
 
-            <div className={classes.logo}>
+            <div className={classes.navbtn}>
                 <Link href="/Sellers">Sellers  </Link>
             </div>
 
-            <div className={classes.logo}>
+            <div className={classes.navbtn}>
                 <Link href="/api/flats">Flats  </Link>
             </div>
 
 
-            <div className={classes.logo}>
+            <div className={classes.navbtn}>
                 <Link href="/">Search  </Link>
             </div>
 
-            <div className={classes.logo}>
+            <div className={classes.navbtn}>
                 <Link href="/login">Login  </Link>
             </div>
 
