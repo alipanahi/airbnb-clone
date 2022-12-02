bd53b233d5a2942e4ca67e095e98370fba87bc35
sajadoncode <sajadoncode@gmail.com>
2022-12-02 09:05:52 +0100

    setup bootstrap to nav

--- components/layout.js/main-header.js
+++ components/layout.js/main-header.js
@@ -1,45 +1,83 @@
-import Link from 'next/link';
-import classes from './main-header.module.css';
-import 'bootstrap/dist/css/bootstrap.css'
-
-import { signIn, signOut } from "next-auth/react"
+import Link from "next/link";
+import { signIn, signOut } from "next-auth/react";
 
 function MainHeader(props) {
-    return (
-        <navbar className={classes.navbar}>
-
-            <div className={classes.airbnbLogo}>
-                <Link href="/">Airbnb </Link>
-            </div>
-            <div className={classes.navbtn}>
-                <Link href="/profile">Profile  </Link>
-            </div>
-
-            <div className={classes.navbtn}>
-                <Link href={ `/sellers` }>Sellers  </Link>
-            </div>
-
-            <div className={classes.navbtn}>
-                <Link href={ `/flats` }>Flats  </Link>
-            </div>
-
-
-            <div className={classes.navbtn}>
-                <Link href="/">Search  </Link>
-            </div>
-
-            <div className={classes.navbtn}>
-                {props.currentUser ? <button onClick={() => signOut()}>Sign out</button> : <button onClick={() => signIn()}>Sign in</button>}
-                
-            </div>
-
-
-            <div>
-                <nav className={classes.navbar}>
-                </nav>
-            </div>
-
-        </navbar>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className={`navbar-brand`} href={`/home`}>
+          Airbnb
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                href={`/profile`}
+              >
+                Profile
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                href={`/sellers`}
+              >
+                Sellers
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                href={`/flats`}
+              >
+                Flats
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                href={`/search`}
+              >
+                Search
+              </Link>
+            </li>
+          </ul>
+          <div>
+            {props.currentUser ? (
+              <button
+                className="btn btn-sm btn-primary"
+                onClick={() => signOut()}
+              >
+                Sign out
+              </button>
+            ) : (
+              <button
+                className="btn btn-sm btn-primary"
+                onClick={() => signIn()}
+              >
+                Sign in
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 export default MainHeader;
