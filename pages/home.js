984e726a59017bb8d0235c1147aab15994468f62
ali h panahi <p.alihussain@gmail.com>
2022-12-02 14:30:07 +0100

    Update home.js
    conflict resolved

--- pages/home.js
+++ pages/home.js
@@ -5,8 +5,7 @@ import flatController from '../controllers/flatController'
 import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 import Link from "next/link";
-import Map from "../components/map";
-
+import Flat from "../components/flat";
 
 export default function Home({flats}) {
   return (
@@ -14,47 +13,23 @@ export default function Home({flats}) {
       <MainHeader />
       <header>
         <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
-          <h1 class="display-4 fw-normal">Airbnb</h1>
+          <h1 class="display-4 fw-normal">Welcome to Airbnb</h1>
           <p class="fs-5 text-muted">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
         </div>
 
         <main>
           <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
             { flats.map(flat => 
-              <div key={flat.id} class="col">
-                <div class="card mb-4 rounded-3 shadow-sm">
-                  <div class="card-header py-3">
-                    <h4 class="my-0 fw-normal">{ flat.name }</h4>
-                  </div>
-                  <div class="card-body">
-                    <h1 class="card-title pricing-card-title">${ flat.price }<small class="text-muted fw-light">/night</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                      <li>{ flat.address }</li>
-                      <li>{ flat.booked }</li>
-                      <li>{ flat.category }</li>
-                      <li>{ flat.rooms }</li>
-                    </ul>
-
-                  </div>
-                </div>
-              </div>
+              <Flat key={flat.id} flat={flat}/>
             )}
           </div>
 
           <Link href={`/flats`}>
-            <Map />
             <h2 class="display-6 text-center mb-4">More...</h2>
           </Link>
-         
         </main>
       </header>
-
-      
-
     </div>
-    
-
-
   )
 
 
